46ee465e766f1d8799a9f6a3c9c5b6c7958da738
Daniel <294.daniel@gmail.com>
2025-11-26 01:12:53 +0100

    Fix rearrangement would make no change

--- TODO.md
+++ TODO.md
@@ -5,7 +5,6 @@
   - insertion page below source file
   - start and end pages below each insertion file
 
-- rearrange file where new_pos is within moving interval
 - check if streamlit gui makes correct validation for each feature
 
 # DONE
@@ -15,3 +14,4 @@
 - modify features to work with uploaded_file objects instead of file path strings
 - fix start and end validation
 - remove page checking from feature scripts: streamlit will make validation
+- rearrange file where new_pos is within moving interval
--- app.py
+++ app.py
@@ -51,16 +51,20 @@ functions = {
     'remove' : remove_pages
 }
 
-# Initialize a counter to force widget recreation
-def reset_interval_widgets():
-    st.session_state['start_widget_counter'] += 1
-    st.session_state['end_widget_counter'] += 1
-
 # initialize keys
 if 'start_widget_counter' not in st.session_state:  
         st.session_state['start_widget_counter'] = 0
 if 'end_widget_counter' not in st.session_state:
         st.session_state['end_widget_counter'] = 0
+
+
+## function to modify widget key to force widget recreation
+def reset_interval_widgets():
+    st.session_state['start_widget_counter'] += 1
+    st.session_state['end_widget_counter'] += 1
+
+
+## function to insert start and end number_input widgets
 def interval_pages_widgets(pdf_file_length):
     col1, col2 = st.columns(2)
     with col1:
@@ -74,6 +78,8 @@ def interval_pages_widgets(pdf_file_length):
     
     return start, end
 
+
+# function to reset file uploader
 def reset_uploader_key():
     if 'file_uploader_key' in st.session_state:
         st.session_state['file_uploader_key'] += 1
@@ -86,6 +92,7 @@ pdf_action = st.sidebar.radio("Actions available:",
         on_change = reset_uploader_key
 )
 
+## function to tailor file uploader based on feature
 def upload_files(action):
     # Initialize the key for upload_file in session state
     if 'file_uploader_key' not in st.session_state:
@@ -134,7 +141,8 @@ if pdf_action:
         else:
             function_docstring = function_docstring.strip()
             break
-
+    
+    # show the action description
     if st.toggle('Show function description'):
         st.write(function_docstring)
     
@@ -142,22 +150,29 @@ if pdf_action:
     uploaded_files = upload_files(action)
 
     # if there are uploaded files
+    # create the start and end pages widgets
     if any(uploaded_files.values()):
 
-        # design upload and page limits
+        # start and end pages widget
         # for extract, remove and rearrange actions
         if action in ['extract', 'remove', 'rearrange']:
+
+            # only one file was uploaded
             uploaded_file = uploaded_files['files'][0]
 
+            # work out the number of pages of pdf file
             pdf_file_length = len(PdfReader(uploaded_file).pages)
             
+            # write start and end widgets' header
             if action != 'extract':
                 st.write(f'Select interval of pages to be {action}d.')
             else:
                 st.write(f'Select interval of pages to be {action}ed.')
 
+            # add start and end widgets
             start, end = interval_pages_widgets(pdf_file_length)
-            
+        
+        # add insert position and relative position widgets for rearrange action
         if action == 'rearrange':
             col1, col2 = st.columns(2)
             with col1:
@@ -165,9 +180,11 @@ if pdf_action:
             with col2:
                 new_pos = st.number_input(label = 'New position', min_value=1, max_value=pdf_file_length,
                                             step = 1, value = 1)
-            if new_pos in range(start, end + 1):
-                st.warning("Rearrangement would not produce any change. PDF file will not be processed.")
-                st.write(f'Insertion page should be before page {start - 1} or after page {end+1}.')
+            same_file_warning = ( new_pos in range(start, end+1) or \
+                                                 (relative_pos == 'after' and start == new_pos + 1) )
+            if same_file_warning:
+                st.warning(f"Rearrangement would not produce any change. PDF file will not be processed. \
+                            \n\nInsertion page should be before page {start - 1} or after page {end+1}.")
 
                     
 
@@ -222,15 +239,18 @@ if pdf_action:
                     
                     elif action == 'rearrange':
                         if interval_ok:
-                            # when new position for extracted pages is within extracted pages range
-                            # the rearranged file would be same as input file
-                            # in this case do nothing
-                            rearranged_file_equals_input_file = new_pos in range(start, end+1)
+                            # There are two cases when rearranged file would
+                            # be same as input file:
+                            # 1. when new position for extracted pages is within extracted pages range
+                            # 2. When moving start page is just above insert point and relative pos is after
+                            # in both cases, do not process file
+                            same_file_warning = ( new_pos in range(start, end+1) or \
+                                                 (relative_pos == 'after' and start == new_pos + 1) )
                             
-                            if not rearranged_file_equals_input_file:
+                            if not same_file_warning:
                                 output_buffer, output_filename = function(uploaded_files['files'][0],
                                                                           start, end, relative_pos, new_pos)
-                            elif rearranged_file_equals_input_file:
+                            elif same_file_warning:
                                 interval_ok = False
                     
                     elif action == 'insert':
@@ -254,9 +274,9 @@ if pdf_action:
         elif action_button_clicked and (start > end):
             st.error('Error: end page lower than start page. Action canceled')
         
-        elif action_button_clicked and action == "rearrange" and not interval_ok:
-            st.warning('Rearranged file would be same as input file. Action canceled.')
+        elif action_button_clicked and action == "rearrange" and same_file_warning:
+            st.warning('Action canceled. Observe warning message above.')
 
-        if action_button_clicked and (start <= end):
+        if action_button_clicked and (start <= end) and not same_file_warning:
             st.success('Processed file ready to be downloaded!')
             
